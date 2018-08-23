9f80390a19273a610ce44b8adc0703b73f6257ed
yyf <874294672@qq.com>
2018-08-23 16:01:22 +0800

    java基础

---
+++ TestForBasis/DataType/BaozhuangType.java
@@ -0,0 +1,15 @@
+package DataType;
+/*
+ * writer:yyf time:2018/08/16
+ */
+public class BaozhuangType {
+	//byte字节 char字符
+	byte a = 68;
+	char b = 'A';
+	/*
+	 * 
+	 */
+	public static void main(String [] args) {
+		
+	}
+}
